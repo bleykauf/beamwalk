3aed4efd69f174719e0f8a3ba97eda1f5d563553
Bastian Leykauf <leykauf@physik.hu-berlin.de>
2021-03-17 18:16:26 +0100

    Add LabDataService as backend.

--- beamwalk/backends.py
+++ beamwalk/backends.py
@@ -1,11 +1,12 @@
-from ThorlabsPM100 import ThorlabsPM100, usbtmc
+from ThorlabsPM100 import ThorlabsPM100 as ThorlabsPM100_
+from ThorlabsPM100 import usbtmc
 import pyvisa as visa
 import os
 import platform
 from time import sleep
 
 
-class PowerMeter:
+class ThorlabsPM100:
     def __init__(self):
 
         self.instr = None
@@ -29,7 +30,7 @@ class PowerMeter:
                 if resource.startswith("USB0::0x1313::0x807"):
                     print(f"Found Powermeter {resource}")
                     instr = rm.open_resource(resource, timeout=1000)
-                    self.instr = ThorlabsPM100(inst=instr)
+                    self.instr = ThorlabsPM100_(inst=instr)
                     print(f"Connected to: {resource}")
                     break
 
@@ -41,3 +42,53 @@ class PowerMeter:
 
     def read(self):
         return self.instr.read
+
+
+class LabDataService:
+    def __init__(self, netloc, field):
+
+        import rcyp
+
+        self.field = field
+        try:
+            host, port = parse_netloc(netloc)
+            self.service = rpyc.connect(host, port)
+            print(
+                f"Connected to {self.service.root.get_service_name()} on port {port}."
+            )
+        except ConnectionRefusedError:
+            print(f"Connection to service at {host}:{port} refused.")
+
+    def read(self):
+        data = self.service.root.exposed_get_data(self.field)
+        val = data["fields"][self.field]
+        return val
+
+
+def parse_netloc(netloc):
+    """
+    Split network location pair hostname:port into the hostname and port.
+
+    Parameters
+    ----------
+    netloc : str or int
+        Network location, e.g. localhost:18861. If an int is passed, localhost is
+        assumed.
+
+    Returns
+    -------
+    host : str
+    port : int
+    """
+    split_netloc = netloc.split(":")
+    if len(split_netloc) == 2:
+        # host:port pair
+        host = split_netloc[0]
+        port = int(split_netloc[1])
+    elif len(split_netloc) == 1:
+        # only port
+        host = "localhost"
+        port = int(split_netloc[0])
+    else:
+        raise ValueError("'{}' is not a valid location".format(netloc))
+    return host, port
